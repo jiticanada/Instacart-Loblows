7c4460574f465bf3a3223c49351bfd561256b81f
jiticanada <110494195+jiticanada@users.noreply.github.com>
2022-08-03 09:27:12 -0400

    Update StoresAndDeliveryTimesPage.java

--- src/main/java/com/instacart/springbootcucumber/pages/StoresAndDeliveryTimesPage.java
+++ src/main/java/com/instacart/springbootcucumber/pages/StoresAndDeliveryTimesPage.java
@@ -1,28 +1,52 @@
-package io.tpd.springbootcucumber;
+package com.instacart.springbootcucumber.pages;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public final class Bag {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-    private final ArrayList<String> things;
+import com.instacart.springbootcucumber.elements.StoresAndDeliveryTimesElements;
 
-    Bag() {
-        things = new ArrayList<>();
-    }
+import graphql.Assert;
 
-    void add(final String something) {
-        things.add(something);
-    }
+public final class StoresAndDeliveryTimesPage {
 
-    public ArrayList<String> getThings() {
-        return things;
-    }
+	WebDriver driver;
+	StoresAndDeliveryTimesElements storesAndDelTimesElements = new StoresAndDeliveryTimesElements();
 
-    public boolean isEmpty() {
-        return things.isEmpty();
-    }
 
-    public void removeEverything() {
-        things.clear();
-    }
+	public void verifyHome()
+	{
+		Assert.assertTrue(storesAndDelTimesElements.showAllBtn.isDisplayed());
+	}
+
+
+	public void clickShowAll() 
+	{
+		storesAndDelTimesElements.showAllBtn.click();
+	}
+
+
+	public void getStoreNamesAndDeliveryTimes() 
+	{
+
+		Map<String, String> store_Time = new HashMap<String, String>();
+
+		List<WebElement> fullStoresList = driver.findElements(By.xpath(storesAndDelTimesElements.totalStores));
+
+		for (int i = 1; i <= fullStoresList.size(); i++) 
+		{
+			String storeName = driver.findElement(By.xpath("//ul[@class='css-10xe3gn']/li[i]/descendant::p[1]")).getText();
+			String storeDeliveryTime = driver.findElement(By.xpath("//ul[@class='css-10xe3gn']/li[i]/descendant::p[2]")).getText();
+
+			store_Time.put(storeName, storeDeliveryTime);
+
+			System.out.println(storeName + "  "+ storeDeliveryTime);
+
+		}
+	}
 }
